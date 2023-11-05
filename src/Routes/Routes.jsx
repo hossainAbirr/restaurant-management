697ca6a6e71e442c9332b957c2945864ef1bbc95
hossainAbirr <hossainabirbusiness@gmail.com>
2023-11-05 09:04:34 +0600

    Router has been added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,22 @@
+
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layouts/MainLayout';
+import Home from '../Pages/Home/Home';
+
+const myRouterForRestaurant = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                
+            }
+        ]
+    },
+]);
+
+export default myRouterForRestaurant
