e768421af9f1b8bc5a76b35e76285b3f8d5c5893
hossainAbirr <hossainabirbusiness@gmail.com>
2023-11-09 17:49:46 +0600

    feedback section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import signature from '../../assets/signature.png';
 import { useEffect, useState } from "react";
 import axios from "axios";
 import TopFood from "./TopFood";
+import Lottie from "lottie-react";
+import contactus from '../../assets/contactus.json';
 const Home = () => {
 
     const [topFoods, setTopFoods] = useState([]);
@@ -44,23 +46,24 @@ const Home = () => {
             <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32">
                 <div className="flex flex-col justify-between">
                     <div className="space-y-2">
-                        <h2 className="text-4xl font-bold leadi lg:text-5xl">Let's talk!</h2>
-                        <div className="text-gray-400">Vivamus in nisl metus? Phasellus.</div>
+                        <h2 className="text-4xl font-bold leadi lg:text-4xl">We&apos;re always open to feedback</h2>
+                        <div className="text-gray-400">Feel free to send your feedback.</div>
                     </div>
-                    <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+                    <Lottie animationData={contactus} loop={true}></Lottie>
+
                 </div>
                 <form className="space-y-6">
                     <div>
                         <label type="name" className="text-sm">Full name</label>
-                        <input id="name" type="text" placeholder="" className="w-full p-3 rounded bg-gray-800" />
+                        <input id="name" type="text" placeholder="" className="w-full p-3 rounded " />
                     </div>
                     <div>
                         <label type="email" className="text-sm">Email</label>
-                        <input id="email" type="email" className="w-full p-3 rounded bg-gray-800" />
+                        <input id="email" type="email" className="w-full p-3 rounded " />
                     </div>
                     <div>
                         <label type="message" className="text-sm">Message</label>
-                        <textarea id="message" rows="3" className="w-full p-3 rounded bg-gray-800"></textarea>
+                        <textarea id="message" rows="3" className="w-full p-3 rounded "></textarea>
                     </div>
                     <button type="submit" className="w-full p-3 text-sm font-bold tracki uppercase rounded bg-violet-400 text-gray-900">Send Message</button>
                 </form>
